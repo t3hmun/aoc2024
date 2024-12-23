1df4c69d59352173a30e75f619a2f15af22dfee8
t3hmun <git@t3hmun.com>
2024-12-23 17:17:03 +0000

    11.2

--- 11/11.ts
+++ 11/11.ts
@@ -4,7 +4,22 @@ const data = Deno.readTextFileSync(
   fromFileUrl(join(dirname(Deno.mainModule), "input")),
 ).replaceAll("\n", "");
 
+// Well this was fun
+// Started by using a linked list becase I though re-arranging an array would get too CPU expensive.
+// That solved part one just fine.
+// For part 2 I ran out of memory, I could have upped to allowing deno to use 64gb but figure that was too much.
+// Then I had the epiphany that the question doesn't care about state just the total so I switched to a depth first recursive algorithm.
+// So that solved the memory issue but it was calling the function ~25mil times a second with no end in sight.
+// So I did searched google, saw the word cache and and went and added one... Finished in less than a second.
+
+//    Trl Bil Mil
+// 252,442,982,856,820
+
+// So if I had just let the non-cached version run then it would have taken about a million seconds.
+
 // Order doesn't change.. but actually it is never considered at all; which means order does not exist.
+
+let cache: Record<number, Record<number, number>> = {};
 
 const nums = data.split(" ").map(Number);
 let calls = 0;
@@ -31,19 +46,34 @@ function Solve(
   limit: number,
 ): number {
   if (blinks > limit) return 1;
+  const l1 = cache[val];
+  if (l1 === undefined) {
+    cache[val] = {};
+  } else {
+    const l2 = l1[blinks];
+    if (l2 !== undefined) return l2;
+  }
+
   report();
-  if (val === 0) return Solve(1, blinks + 1, limit);
+  if (val === 0) {
+    const res = Solve(1, blinks + 1, limit);
+    cache[val][blinks] = res;
+    return res;
+  }
   const str = String(val);
   const len = str.length;
   if ((len & 1) === 0) {
-    let lrTotal = 0;
+    let res = 0;
     const left = Number(str.slice(0, len / 2));
     const right = Number(str.slice(len / 2));
-    lrTotal += Solve(left, blinks + 1, limit);
-    lrTotal += Solve(right, blinks + 1, limit);
-    return lrTotal;
+    res += Solve(left, blinks + 1, limit);
+    res += Solve(right, blinks + 1, limit);
+    cache[val][blinks] = res;
+    return res;
   }
-  return Solve(val * 2024, blinks + 1, limit);
+  const res = Solve(val * 2024, blinks + 1, limit);
+  cache[val][blinks] = res;
+  return res;
 }
 
 let part1 = 0;
@@ -56,6 +86,7 @@ console.log(`Calls: ${calls}`);
 console.log(`Part1: ${part1}`);
 
 let part2 = 0;
+cache = {};
 calls = 0;
 target = step;
 start = Date.now();
@@ -65,5 +96,5 @@ for (const num of nums) {
   console.log(`${num} solved in ${Date.now() - now}ms`);
   part2 += count;
 }
-console.log(`Calls: ${calls}`);
+console.log(`Calls: ${calls}, cache len: ${Object.keys(cache).length}`);
 console.log(`Part2: ${part2}`);
